b0ae48671843698a229cd44765f93ad624a39994
Sándor Balázs <balazs.sandor@vcc.live>
2018-07-12 06:10:45 +0200

    camera: add mjpeg http server

--- camera/camera.py
+++ camera/camera.py
@@ -1,3 +1,159 @@
-import cv2
+import http
+import multiprocessing
+import os
+from threading import Thread
 
-print(cv2.__version__)
+import cv2
+import time
+import argparse
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn
+
+from multiprocessing import Queue, Process
+
+CWD_PATH = os.getcwd()
+
+class CamHandler(BaseHTTPRequestHandler):
+
+    def __init__(self, request, client_address, server):
+        img_src = 'http://{}:{}/cam.mjpg'.format(server.server_address[0], server.server_address[1])
+        self.html_page = """
+            <html>
+                <head></head>
+                <body>
+                    <img src="{}"/>
+                </body>
+            </html>""".format(img_src)
+        self.html_404_page = """
+            <html>
+                <head></head>
+                <body>
+                    <h1>NOT FOUND</h1>
+                </body>
+            </html>"""
+        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
+
+    def do_GET(self):
+        if self.path.endswith('.mjpg'):
+            self.send_response(http.HTTPStatus.OK)
+            self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
+            self.end_headers()
+            while True:
+                try:
+                    # img = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
+                    img = output_q.get()
+                    retval, jpg = cv2.imencode('.jpg', img)
+                    if not retval:
+                        raise RuntimeError('Could not encode img to JPEG')
+                    jpg_bytes = jpg.tobytes()
+
+                    self.wfile.write("--jpgboundary\r\n".encode())
+                    self.send_header('Content-type', 'image/jpeg')
+                    self.send_header('Content-length', len(jpg_bytes))
+                    self.end_headers()
+                    self.wfile.write(jpg_bytes)
+                except (IOError, ConnectionError):
+                    break
+        elif self.path.endswith('.html'):
+            self.send_response(http.HTTPStatus.OK)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(self.html_page.encode())
+        else:
+            self.send_response(http.HTTPStatus.NOT_FOUND)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(self.html_404_page.encode())
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+
+def start_http_server():
+    server = ThreadedHTTPServer(('0.0.0.0', 8080), CamHandler)
+    try:
+        print("server started 8080")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.socket.close()
+
+
+class WebcamVideoStream:
+    def __init__(self, src, width, height):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src', '--source', dest='video_source', type=int,
+                        default=0, help='Device index of the camera.')
+    parser.add_argument('-wd', '--width', dest='width', type=int,
+                        default=160, help='Width of the frames in the video stream.')
+    parser.add_argument('-ht', '--height', dest='height', type=int,
+                        default=120, help='Height of the frames in the video stream.')
+    parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
+                        default=1, help='Number of workers.')
+    parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
+                        default=3, help='Size of the queue.')
+    args = parser.parse_args()
+
+    logger = multiprocessing.log_to_stderr()
+    # logger.setLevel(multiprocessing.SUBDEBUG)
+
+    input_q = Queue(maxsize=args.queue_size)
+    output_q = Queue(maxsize=args.queue_size)
+
+    video_capture = WebcamVideoStream(src=args.video_source,
+                                      width=args.width,
+                                      height=args.height).start()
+
+    p = Process(target=start_http_server)
+    p.start()
+
+    try:
+        while True:
+            frame = video_capture.read()
+            output_q.put(frame)
+            time.sleep(0.5)
+
+    except KeyboardInterrupt:
+        pass
+
+    video_capture.stop()
+    print('terminate server process')
+    p.join(1)
+    p.terminate()
